90b3a65c8dfe1da0bda39dfd5907ae2d02dc2003
Nick <hansen.nick95@gmail.com>
2019-10-17 20:46:51 -0700

    finished login and user creation forms (inclusing post requests)

---
+++ nba/src/components/AccountCreation.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import useForm from 'react-hook-form';
+import axios from 'axios'
+
+
+const AccountCreation = () => {
+
+  const {register, handleSubmit} = useForm();
+  
+  const onSubmit = (values) => {
+    console.log(values);
+    axios.post('https://nba-predictor-ptbw.herokuapp.com/register', {'email': values.email, 'password': values.password})
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
+  return(
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Email</label>
+      <input type="text" ref={register} name='email' />
+
+      <label>Password</label>
+      <input type="password" ref={register} name='password' />
+
+      <button type='submit'>Submit!</button>
+    </form>
+  )
+}
+
+export default AccountCreation
