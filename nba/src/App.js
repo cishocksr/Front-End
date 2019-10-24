6a6e8609292eead01ce12d14bd813e9522d34b67
cishocksr <cishockleysr@gmail.com>
2019-10-24 15:10:18 -0400

    search and beginning style

--- nba/src/App.js
+++ nba/src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
-import AccountCreation from './components/AccountCreation';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login';
+import "./App.css";
+import AccountCreation from "./components/AccountCreation";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h2>
-            Welcome to NBA Career Longevity Projector
-          </h2>
-          <Link to='/login'>
+          <h2>Welcome to NBA Career Longevity Projector</h2>
+          <Link to="/login">
             <p>Log In Now!</p>
           </Link>
           <Route exact path="/login" component={Login} />
-          <Link to='/account'>
+          <Link to="/account">
             <p>Sign Up Now!</p>
           </Link>
-          <PrivateRoute path='/account' component = {AccountCreation} />
-
+          <PrivateRoute path="/account" component={AccountCreation} />
         </header>
       </div>
     </Router>
@@ -29,5 +26,3 @@ function App() {
 }
 
 export default App;
-
-
